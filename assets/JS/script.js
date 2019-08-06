fa136e9709539e48a61a620e17f263a02c1a380c
Dave Ward <professorx00@gmail.com>
2019-08-06 14:13:15 -0400

    Functional Connection to Firebase for Database Storage

---
+++ assets/JS/script.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA77eFLbhqDysttRaxEYoLoDBr92TaxW-A",
+    authDomain: "traintimelogger.firebaseapp.com",
+    databaseURL: "https://traintimelogger.firebaseio.com",
+    projectId: "traintimelogger",
+    // storageBucket: "traintimelogger.appspot.com",
+    messagingSenderId: "158876298270",
+    appId: "1:158876298270:web:2baf482592a7ccfd"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+
+// database.ref('/Testing').push("True");
+// console.log("finished")
